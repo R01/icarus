60ae4108187e491d478ba957475aeac33d3dc6c3
richard <richard@inglis.net>
2011-03-25 10:35:52 +0000

    First Commit

---
+++ icarusproject/src/icaruspackage/eurobot.java
@@ -0,0 +1,15 @@
+package icaruspackage;
+
+import lejos.nxt.Button;
+
+public class eurobot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Button.waitForPress();
+	}
+
+}
