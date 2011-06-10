6b4d685a79997c6cb379d09d8e41dfe30849a367
Harry Eakins <harry.eakins@googlemail.com>
2011-06-10 21:11:19 +0100

    Cleaned up some parts of the code

--- icarusproject/src/icaruspackage/Eurobot.java
+++ icarusproject/src/icaruspackage/Eurobot.java
@@ -7,44 +7,50 @@ import lejos.util.Timer;
 import lejos.util.TimerListener;
 
 public class Eurobot {
-	static int MIN_DISTANCE = 13;
-	static int MATCH_LENGTH = 90000; // 90 seconds
-	static int speed = 10;
+	/** Measured constants you probably shouldn't change **/
+	static final int MATCH_LENGTH = 90000; // milliseconds
+	static final float WHEEL_DIAMETER = 8.0f;
+	static final float WHEEL_BASE = 30.5f;
 	static boolean competition = false;
-	static boolean obstacle = false;
-	static DifferentialPilot pilot;
+	
+	/** Trial and error constants you can play with **/
+	static final int AVOIDANCE_THRESHOLD = 13; // cm
+	static final int speed = 15; // cm/sec
+	
+	static DifferentialPilot pilot = new DifferentialPilot(WHEEL_DIAMETER, // cm
+															WHEEL_BASE, // cm
+															Motor.B, Motor.C, true);
 	static TouchSensor bump = new TouchSensor(SensorPort.S3);
 	static TouchSensor pawn = new TouchSensor(SensorPort.S4);
 	static UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S1);
 	static ColorSensor light = new ColorSensor(SensorPort.S2);
 
-	public static void main(String[] args) {
-		// ## Initialization #######################
-		pilot = new DifferentialPilot(8.0f, 30.5f, Motor.B, Motor.C, true);
+	public static void main(String[] args) {		
+		/** More initialization ***********************/
 		pilot.setTravelSpeed(speed);
+		pilot.setRotateSpeed(speed*Math.PI*WHEEL_BASE/180.0f); // degrees/sec#
+		
+		registerStopButtonInterrupt();
 		Timer matchTimer = initMatchTimer();
-		sonic.continuous();
-		Timer avoidance = initSonicAvoidance();
-		avoidance.start();
-//		TODO: init 90 sec counter
-		initStopButton();
+		startSonicAvoidanceThread();
+		footUp(); // Just in case!
 		
 		// wait for start signal:
-		while(getColorString(light.getColorID()).equals("BLACK")){competition = true;}
-		// GO! start the 90 sec match timer
+		while(light.getColorID() == Color.BLACK){competition = true;}
 		matchTimer.start();
+		
 		// wait 300ms to make sure the starting paper is clear of the colour sensor.
 		lejos.util.Delay.msDelay(300);
-		// get the start colour, pass it as an argument to the main go() method
-		LCD.clearDisplay();
-		LCD.drawString(getColorString(light.getColorID()),0,0,false);
 		
-		go(getColorString(light.getColorID()));
-		// clean up
-		avoidance.stop();
+		// get the start colour, pass it as an argument to the main go() method
+		int color;
+		do {
+			 color = light.getColorID();
+		}while(color != Color.RED && color != Color.BLUE);
+		go(color);
 	}
 
-	public static void initStopButton(){
+	public static void registerStopButtonInterrupt(){
 		SensorPort.S3.addSensorPortListener(new SensorPortListener() {
 			public void stateChanged(SensorPort port, int oldValue, int newValue){
 				if (bump.isPressed()) {
@@ -52,28 +58,31 @@ public class Eurobot {
 					Motor.A.stop();
 					Motor.B.stop();
 					Motor.C.stop();
-					NXT.exit(0);
+					NXT.shutDown();
 				}}});	
 
 	}
 
-	public static Timer initSonicAvoidance(){
+	public static void startSonicAvoidanceThread(){
+		// Make sonar continuously measure distances
+		sonic.continuous();
+		
+		// Poll the sensor for the latest reading and stop if obstacle
 		TimerListener tl = new TimerListener()
 		{		   
 			public void timedOut(){
-				LCD.drawInt(light.getColorID(),6,0,0);//NEW LINE
-				if(!obstacle && sonic.getDistance()<MIN_DISTANCE){
-					obstacle = true;
+				LCD.clear(1);
+				LCD.drawInt(light.getColorID(),0,1);//NEW LINE
+				if(sonic.getDistance() < AVOIDANCE_THRESHOLD){
 					pilot.setTravelSpeed(0);
-				} else if(obstacle && sonic.getDistance()>=MIN_DISTANCE) {
-					obstacle = false;
+				} else if(sonic.getDistance() >= AVOIDANCE_THRESHOLD) {
 					pilot.setTravelSpeed(speed);
 				}
 			}   
 		};
 		// set the timer event to fire every 500ms
 		Timer timer = new Timer(500,tl);	   
-		return timer;
+		timer.start();
 	}
 
 	public static Timer initMatchTimer(){
@@ -81,22 +90,20 @@ public class Eurobot {
 		{		   
 			public void timedOut(){
 				// Match over: switch off...
-				NXT.exit(0);
+				NXT.shutDown();
 			}   
 		};
 		// set the timer event to fire after MATCH_LENGTH
-		Timer timer = new Timer(MATCH_LENGTH,tl);	   
+		Timer timer = new Timer(MATCH_LENGTH,tl);	
 		return timer;
 	}
-
-	public static String otherColor(String thisColor){
-		if(thisColor.equals("RED")) return "BLUE";
-		if(thisColor.equals("BLUE")) return "RED";
-		LCD.drawString("ERROR", 0, 0,true);
-		lejos.util.Delay.msDelay(10000);
-		return"";
+	
+	public static int opposite(int color) {
+		if(color == Color.BLUE) return Color.RED;
+		else if(color == Color.RED) return Color.BLUE; 
+		else return -1;
 	}
-
+	
 	public void go2(String startColor){
 		// *** NEW STRATEGY FOR THE FINALS ***
 		
@@ -146,67 +153,84 @@ public class Eurobot {
 	}
 	
 	
-	public static void go(String startColor) {
-		int turnFactor = startColor.equals("BLUE")?1:-1;
+	public static void go(int startColor) {
+		int turnFactor = (startColor == Color.BLUE)?1:-1;
+		
+		// Move out of the starting box
 		pilot.travel(100, true);
 		while (pilot.isMoving()) {
-			if (getColorString(light.getColorID()).equals(otherColor(startColor))) pilot.stop();
+			if (light.getColorID() != startColor) pilot.stop();
 		}
-		
-		//pilot.rotate(90);
-		pilot.arc(turnFactor*20.0f,turnFactor*90.0f);
+	
 		pilot.reset();
+	
+		// Turn onto the first line
+		pilot.arc(turnFactor*20.0f,turnFactor*90.0f);
+		
+		pilot.reset();
+		
+		// Drive forwards until you find a pawn
 		pilot.travel(200, true);
 		while (pilot.isMoving()) {
-			if (pawn.isPressed()) pilot.stop();//Found a pawn!
+			if (pawn.isPressed()) pilot.stop(); //Found a pawn!
 		}
+		// Remember how far you drove
 		float travel2 = pilot.getMovement().getDistanceTraveled();
 
-		pilot.setRotateSpeed(90.0f);
-		pilot.rotate(180); // turn round and go home
-		
-		pilot.setTravelSpeed(speed);
+		// Turn round and go home
+		pilot.rotate(180); 
 		pilot.travel(travel2+22.0f, false);
 		pilot.rotate(-90*turnFactor);
 		pilot.travel(60, true);
-		while (pilot.isMoving()) {
-			if (getColorString(light.getColorID()).equals("BLACK")) {
-				lejos.util.Delay.msDelay(100);
-				if (getColorString(light.getColorID()).equals("BLACK")) {
-					lejos.util.Delay.msDelay(100);
-					if (getColorString(light.getColorID()).equals("BLACK")) {
-						pilot.stop();
-					}
-				}
-			}
-		}
+
+		// Go past black
+		int n = 0;
+		do {
+			if(light.getColorID() == Color.BLACK) ++n;
+			else n = 0;
+			lejos.util.Delay.msDelay(100);
+		} while(n < 3);
+			
+		// Go a little bit further
 		if(competition) {
-			pilot.travel(12, false);
+			pilot.travel(12);
 		} else {
-			pilot.travel(5, false);
+			pilot.travel(5);
 		}
-		lift(500);
-		// REMOVE THIS FOR THE COMPETITION:
+		
+		footDown();
+		
 		if(!competition) {
 			lejos.util.Delay.msDelay(4000);
-			lift(-500);
+			footUp();
 			pilot.setTravelSpeed(speed);
 			pilot.rotate(180);
 		} else {
-			NXT.exit(0);
+			NXT.shutDown();
 		}
 	}
 
-	static void lift(int distance){
-		Motor.A.resetTachoCount();
-		if(distance>0){
-			Motor.A.backward();
-		} else {
-			Motor.A.forward();
-		}    int count = 0;
-		while( java.lang.Math.abs(count) < java.lang.Math.abs(distance)) count = Motor.A.getTachoCount();         
-		Motor.A.stop();
+	static void moveFoot(int distance) {
+		//Initialize the motor and tell it to rotate for ages
+		Motor.A.setSpeed(400);
+		Motor.A.setStallThreshold(20, 10);
+		Motor.A.rotate(distance, true);
+
+		// Wait for stall
+		Motor.A.waitComplete();
+		
+		//Wind back a bit to relax motor
+		Motor.A.rotate(-20); 
 	}
+	
+	static void footUp(){
+		moveFoot(1000);
+	}
+	
+	static void footDown(){
+		moveFoot(-1000);
+	}
+
 
 	static String getColorString(int v){
 		switch(v){
